753dfefb0025cf3b95f7ab1100ed16e0b72b1864
jhansigangavarapu <jhansigangavarapu1234@gmail.com>
2022-12-03 20:55:25 +0530

    added java demo application

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkifinputiseven(122));
+    }
+    public static boolean checkifinputiseven(int number){
+        return number % 2 == 0;
+    }
+}
